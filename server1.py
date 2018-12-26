d55673711569795edb004612b6c09aac8d5ae1c6
amirtal-12-2018 <46166666+amirtal-12-2018@users.noreply.github.com>
2018-12-26 19:40:54 +0200

    Create server1.py

---
+++ server1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+
+PORT_NUMBER = 80
+
+class myHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes('Hi {}, Welcome!'.format(self.path.strip('/')), 'utf8'))
+        return
+
+def run():
+    httpd = HTTPServer(('0.0.0.0', PORT_NUMBER), myHandler)
+    httpd.serve_forever()
+
+run()
